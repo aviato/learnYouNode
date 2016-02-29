a251b0363ed6e6eeda5b5802e921ba591faa78ec
Ryan Price <rsprice90@gmail.com>
2016-02-28 23:26:24 -0800

    (fix) remove old files

--- babySteps.js
+++
@@ -1,7 +0,0 @@
-var args, i, sum;
-args = process.argv;
-sum  = 0;
-
-for (i = 2; i < args.length; i++) { sum += parseInt(args[i], 10); };
-
-console.log(sum);
--- myFirstIO.js
+++
@@ -1,6 +0,0 @@
-var fs, fileName;
-
-fs = require('fs');
-fileName = process.argv[2];
-
-console.log(fs.readFileSync(fileName, 'utf-8').split('\n').length - 1);
